67ee6fafbf32e63b8349d6e54f7f93f31e11c7e4
kbyyy <1085529498@qq.com>
2016-12-02 00:41:59 +0800

    wangyi

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#fullpage").fullpage({
+        // anchors: ['nub1', 'nub2', 'nub3', 'nub4', 'nub5','nub6','nub7','nub8','nub9','nub10'],
+        // menu: '#mymenu',
+        navigation:true
+
+    });
+})
